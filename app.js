8b2b757b1cfd86978649797390f551b0990f87da
Clive Machado <clive.machado@raweng.com>
2017-12-05 12:28:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('blt0307e22de402c426')
+var app = Built.App('blt6eef1c94c2ce0f4f')
 .setHost("test-stag-api.built.io")
 .setProtocol("https")
-.setMasterKey('blt9e8a0128934e71e9')
+.setMasterKey('blta53058e6ac1fc255')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'blt9b4ba508a72d39e6',
+	secret_key     : 'cliveblt',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
